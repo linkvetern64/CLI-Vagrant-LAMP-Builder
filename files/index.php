b709b625653f481cc9bc79154adcf5905e052aeb
linkvetern64 <jstand1@umbc.edu>
2018-02-18 13:41:32 -0500

    Modifed index

--- files/index.php
+++ files/index.php
@@ -1,3 +1,4 @@
+<?php require_once("../load.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Shiny-meme</title>
+    <title></title>
 
     <!-- FAVICON -->
     <!-- <link rel='shortcut icon' href='img/favicon/favicon.ico' type='image/x-icon'/ > -->
@@ -24,111 +25,11 @@
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
 
-    <!-- Load the Twitch embed script -->
-    <script src="https://embed.twitch.tv/embed/v1.js"></script>
 </head>
 <body>
-
-<!-- Navbar -->
-<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse sticky-top" style="z-index: 100;">
-
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">Welcome</a>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-        </ul>
-
-        <form class="form-inline my-2 my-lg-0">
-                <input class="form-control" type="text" placeholder="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-                    <span class="fa fa-search"></span>
-                </button>
-        </form>
-    </div>
-</nav>
-
 <!-- Main content pane -->
 <div id="container">
 
-    <!-- Welcome Container -->
-    <div id="welcome-container">
-        <!-- Content goes here -->
-        <!-- Add a placeholder for the Twitch embed -->
-        <div id="twitch-embed"></div>
-
-
-        <!-- Create a Twitch.Embed object that will render within the "twitch-embed" root element. -->
-        <script type="text/javascript">
-            new Twitch.Embed("twitch-embed", {
-                width: 854,
-                height: 480,
-                autoplay: false,
-                channel: "retrogaminglivetv"
-            });
-        </script>
-
-
-        <!-- Follow Container -->
-        <div id="follow-container">
-
-            <a href="twitch.tv"> <span id="twitch-hover"  class="fab fa-twitch"></span></a>
-            <a href="twitter.com"> <span id="twitter-hover" class="fab fa-twitter"></span></a>
-            <a href="youtube.com"><span id="youtube-hover" class="fab fa-youtube"></span></a>
-        </div>
-    </div>
-
-    <div id="market-container">
-
-        <!-- Market Cards -->
-        <div class="card-group">
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-block">
-                    <h4 class="card-title">Card title</h4>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-            <div class="card" >
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-block">
-                    <h4 class="card-title">Card title</h4>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
-                <div class="card-block">
-                    <h4 class="card-title">Card title</h4>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-        </div>
-
-    </div>
 </div>
-
-
-
 </body>
 </html>
